16f8fdfc767a583419acf7e95116875e84094040
pejko <aleksandarpejkovic@hotmail.com>
2024-02-04 19:59:20 +0100

    first commit

---
+++ src/main/java/org/example/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,43 @@
+package org.example.config.security;
+
+import org.example.exception.notfound.UserNotFoundException;
+import org.example.exception.security.BlockedRequestException;
+import org.example.repository.UserRepository;
+import org.example.service.LoginAttemptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private LoginAttemptService loginAttemptService;
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        if (loginAttemptService.isBlocked()) {
+            throw new BlockedRequestException("blocked");
+        }
+
+        return userRepository
+                .findByUsername(username)
+                .map(
+                        storedUser -> org.springframework.security.core.userdetails.User.builder()
+                                .username(storedUser.getUsername())
+                                .password(storedUser.getPassword())
+                                .accountExpired(!storedUser.isActive())
+                                .accountLocked(false)
+                                .build()
+                )
+                .orElseThrow(() -> new UserNotFoundException("User not found!"));
+    }
+}
---
+++ src/main/java/org/example/controller/LoginController.java
@@ -0,0 +1,27 @@
+package org.example.controller;
+
+import org.example.service.TokenService;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/api")
+public class LoginController {
+
+    private final TokenService tokenService;
+
+    public LoginController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/login")
+    public String login(Authentication authentication) {
+        log.info("Endpoint '/api/login' was called to authenticate trainee");
+        return tokenService.generateToken(authentication);
+    }
+}
